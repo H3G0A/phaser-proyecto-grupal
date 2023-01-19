48e77a9b4773a434b52cfcbc3043c67d7a568f38
Jorge <jorgezg0103@gmail.com>
2023-01-19 22:13:05 +0100

    Created test scene to verify enemy logic

---
+++ src/scenes/EnemyTestScene.js
@@ -0,0 +1,35 @@
+import Phaser from "phaser"
+import Mummy from '../enemies/Mummy'
+import SamplePlayer from '../SamplePlayer'
+
+export default class HelloWorldScene extends Phaser.Scene {
+	constructor() {
+		super('hello-world')
+	}
+
+	preload() {
+		this.load.image('sea', '../../res/sea.png')
+		this.load.spritesheet('mummy', '../../res/enemies/mummy37x45.png', { frameWidth: 37, frameHeight: 45 });
+		this.load.spritesheet('player', '../../res/player/idle/idle-1.png', { frameWidth: 71, frameHeight: 67 });
+	}
+
+	create() {
+		// Add background
+		this.add.image(400, 300, 'sea').setScale(8);
+
+		// Add player and enemies
+		this.mummy = new Mummy(this, 400, 300, 'mummy');
+		this.player = new SamplePlayer(this, 200, 300, 'player');
+
+		// Set collisions
+		this.physics.add.overlap(this.player, this.mummy, () => { this.player.takeDamage(this.mummy.damage) }, null, this);
+
+	}
+
+	
+
+	update() {
+		this.mummy.update();
+		this.player.update();
+	}
+}
