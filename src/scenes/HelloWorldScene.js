feb2a1569836da3ce3445a6477cf639232c14731
Jorge <jorgezg0103@gmail.com>
2023-01-19 22:10:21 +0100

    Code formatting

--- src/scenes/HelloWorldScene.js
+++ src/scenes/HelloWorldScene.js
@@ -1,48 +1,43 @@
-import Phaser from 'phaser'
+import Phaser from "phaser"
 import Mummy from '../Mummy'
-export default class HelloWorldScene extends Phaser.Scene
-{
-	constructor()
-	{
+export default class HelloWorldScene extends Phaser.Scene {
+	constructor() {
 		super('hello-world')
 	}
 
-	preload()
-    {
-        this.load.setBaseURL('http://labs.phaser.io')
+	preload() {
+		this.load.setBaseURL('http://labs.phaser.io')
 
-        this.load.image('sky', 'assets/skies/space3.png')
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-        this.load.image('red', 'assets/particles/red.png')
-        this.load.spritesheet('mummy','assets/animations/mummy37x45.png',
-        {frameWidth:37, frameHeight:45});
-    }
+		this.load.image('sky', 'assets/skies/space3.png')
+		this.load.image('logo', 'assets/sprites/phaser3-logo.png')
+		this.load.image('red', 'assets/particles/red.png')
+		this.load.spritesheet('mummy', 'assets/animations/mummy37x45.png',
+			{ frameWidth: 37, frameHeight: 45 });
+	}
 
-    create()
-    {
-        this.add.image(400, 300, 'sky')
+	create() {
+		this.add.image(400, 300, 'sky')
 
-        const particles = this.add.particles('red')
+		const particles = this.add.particles('red')
 
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        })
+		const emitter = particles.createEmitter({
+			speed: 100,
+			scale: { start: 1, end: 0 },
+			blendMode: 'ADD'
+		})
 
-        this.mummy01 = new Mummy(this,300,100,'mummy');
+		this.mummy01 = new Mummy(this, 300, 100, 'mummy');
 
-        const logo = this.physics.add.image(400, 100, 'logo')
+		const logo = this.physics.add.image(400, 100, 'logo')
 
-        logo.setVelocity(100, 200)
-        logo.setBounce(1, 1)
-        logo.setCollideWorldBounds(true)
+		logo.setVelocity(100, 200)
+		logo.setBounce(1, 1)
+		logo.setCollideWorldBounds(true)
 
-        emitter.startFollow(logo)
-    }
+		emitter.startFollow(logo)
+	}
 
-    update()
-    {
-        this.mummy01.update();
-    }
+	update() {
+		this.mummy01.update();
+	}
 }
