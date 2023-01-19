60f91c86a69bb54f1ed5d360b879b12849be6c4b
Jorge <jorgezg0103@gmail.com>
2023-01-19 22:12:27 +0100

    Added enemy class and mummy enemy

---
+++ src/enemies/Mummy.js
@@ -0,0 +1,47 @@
+import Phaser from 'phaser'
+import Enemy from './Enemy'
+
+export default class Mummy extends Enemy {
+
+	constructor(scene, x, y, spriteName) {
+		super(scene, x, y, spriteName);
+
+		this.scene.anims.create(
+			{
+				key: 'walk',
+				frames: this.scene.anims.generateFrameNumbers(spriteName, { start: 0, end: 18 }),
+				frameRate: 10,
+				repeat: -1
+			}
+		);
+		this.play('walk');
+
+		this.finishedRoute = false;
+		this.initialPos = this.x;
+
+	}
+
+	patrolling (distance) {
+		let endPos = this.initialPos + distance;
+		if(this.x >= endPos){
+			this.finishedRoute = true;
+		}
+		if(this.x <= this.initialPos){
+			this.finishedRoute = false;
+		}
+		
+		if(!this.finishedRoute){
+			this.setVelocityX(20);
+			this.flipX = false;
+		}
+		else{
+			this.setVelocityX(-20);
+			this.flipX = true;
+		}
+	}
+
+	update() {
+		this.patrolling(100);
+	}
+
+}
