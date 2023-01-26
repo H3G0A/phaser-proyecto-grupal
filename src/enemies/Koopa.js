094ded7064d58a0ae0e2d605f1804214360c8432
Jorge <jorgezg0103@gmail.com>
2023-01-26 20:26:30 +0100

    Added flying enemy

---
+++ src/enemies/Koopa.js
@@ -0,0 +1,44 @@
+import Enemy from './Enemy'
+
+export default class Koopa extends Enemy {
+	constructor(scene, x, y, distance, spriteName) {
+		super(scene, x, y, spriteName);
+
+		this.scene.anims.create(
+			{
+				key: 'fly',
+				frames: this.scene.anims.generateFrameNames(spriteName, {prefix: 'koopa', end: 3, zeroPad: 0}),
+				frameRate: 5,
+				repeat: -1
+			}
+		);
+		this.play('fly');
+		this.body.setAllowGravity(false)
+
+		this.initialPos = this.y;
+		this.finishedRoute = false;
+		this.distance = distance;
+
+	}
+
+	patrolling (distance) {
+		let endPos = this.initialPos + distance;
+		if(this.y >= endPos){
+			this.finishedRoute = true;
+		}
+		if(this.y <= this.initialPos){
+			this.finishedRoute = false;
+		}
+		
+		if(!this.finishedRoute){
+			this.setVelocityY(20);
+		}
+		else{
+			this.setVelocityY(-20);
+		}
+	}
+
+	update(){
+		this.patrolling(this.distance);
+	}
+}
