2e76a24861dbdfbfb72dc854acddabf3a156586f
Raquel Garrido Crespo <raquela82@gmail.com>
2023-01-29 18:24:03 +0100

    Fix walking

--- src/characters/Player.js
+++ src/characters/Player.js
@@ -198,7 +198,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 		else if (this.cursors.down.isDown) {
 			this.setVelocityY(200);
 		}
-		else if (this.cursors.left.isDown) {
+		if (this.cursors.left.isDown && this.cursors.right.isUp) {
 			this.setVelocityX(-200);
 			if (!(this.anims.isPlaying && this.anims.currentAnim.key === 'player-jump')) {
 				this.anims.play('player-walk-left',true).on('animationcomplete', () => {this.anims.play('stay',true)});
@@ -210,7 +210,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 				this.anims.play('stay',true);
 			}
 		}
-		else if (this.cursors.right.isDown) {
+		if (this.cursors.right.isDown) {
 			this.setVelocityX(200);
 			if (!(this.anims.isPlaying && this.anims.currentAnim.key === 'player-jump')) {
 				this.anims.play('player-walk-right',true).on('animationcomplete', () => {this.anims.play('stay',true)});
@@ -222,7 +222,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 				this.anims.play('stay',true);
 			}
 		}
-		else if (this.spaceKey.isDown){
+		if (this.spaceKey.isDown){
 				if (this.anims.isPlaying && this.anims.currentAnim.key === 'player-walk-left') {
 					this.shoot(-1);
 				}else if (this.anims.isPlaying && this.anims.currentAnim.key === 'player-walk-right' || this.anims.currentAnim.key === 'stay') {
@@ -235,7 +235,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 				this.executeSuperShot(1);
 			}
 		}
-		else {
+		else if (this.cursors.left.isUp && this.cursors.right.isUp && this.cursors.down.isUp && this.cursors.up.isUp){
 			this.setVelocityX(0);
 		}
 
